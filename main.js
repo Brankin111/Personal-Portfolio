7a2e247e4a46d5be79d00424897233f51c7b7c96
Brandon Rankin <brankin111@gmail.com>
2020-11-28 17:56:06 -0500

    changed portfolio color scheme. added subtle GSAP animations in main.js

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-//////jQuery Trials//////
+/***** jQuery *****/
 
-$('#welcome-section')
-    .hide()
-    .slideDown(2500);
+//$('#main-heading, p')
+//    .hide()
+//    .fadeIn(2500);
+
+
+/***** GSAP *****/
+
+gsap.from('nav', {
+    duration: 1,
+    y: '-100%',
+    ease: 'bounce'
+})
+
+gsap.from('.nav-list a', {
+    duration: 1,
+    opacity: 0,
+    delay: 1,
+    stagger: .30
+})
+
+gsap.from('.gsap-heading', {
+    duration: 1.75,
+    opacity: 0,
+    scale: 0,
+    delay: 1.75
+})
+
+gsap.from('.projects-grid', {
+    scrollTrigger: {
+        trigger: '.projects-grid',
+        toggleActions: 'restart pause resume restart',
+    },
+    duration: 2.5,
+    //    opacity: 0,
+    x: '100%',
+    delay: .25,
+})
